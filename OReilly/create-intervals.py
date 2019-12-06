37764c846772ca488b6ba1af5192f28e6dc9c0fd
Maxim SInyaev <bemaxradio@gmail.com>
2019-12-06 20:28:32 +0300

    create-intervals.py descriprion updated

--- OReilly/create-intervals.py
+++ OReilly/create-intervals.py
@@ -1,15 +1,21 @@
 #!/home/poligon/.local/bin/checkio --domain=py run create-intervals
 
-# From a set of ints you have to create a list of closed intervals as tuples, so the  intervals are covering all the values found in the set.
+# From a set of ints you have to create a list of closed intervals as tuples, so the
+# intervals are covering all the values found in the set.
 # 
-# A closed interval includes its endpoints! The interval1..5, for example,  includes each valuexthat satifies the condition1<= x<= 5.
+# A closed interval includes its endpoints! The interval1..5, for example,
+# includes each valuexthat satifies the condition1<= x<= 5.
 # 
-# Values can only be in the same interval if the difference between a value and the next  smaller value in the set equals one, otherwise a new interval begins. Of course, the  start value of an interval is excluded from this rule.
-# A single value, that does not fit into an existing interval becomes the start- and  endpoint of a new interval.
+# Values can only be in the same interval if the difference between a value and the next
+# smaller value in the set equals one, otherwise a new interval begins. Of course, the
+# start value of an interval is excluded from this rule.
+# A single value, that does not fit into an existing interval becomes the start- and
+# endpoint of a new interval.
 # 
-# Input:A set of ints.
+# Input: A set of ints.
 # 
-# Output:A list of tuples of two ints, indicating the endpoints of the interval. The Array should be sorted by start point of each interval
+# Output: A list of tuples of two ints, indicating the endpoints of the interval.
+#         The Array should be sorted by start point of each interval
 # 
 # 
 # END_DESC
